c4ce79be18f67f3b019059780a085813f2390f45
Turbanov Valeriy <tur.4000@gmail.com>
2021-02-07 13:46:15 +0200

    подготовка тестов

--- src/Parsers/buildAst.php
+++ src/Parsers/buildAst.php
@@ -13,55 +13,6 @@
 
 namespace Differ\Differ\Parsers;
 
-/*
-function buildAst($data1, $data2)
-{
-    $mergedData = array_merge($data1, $data2);
-    $ast = [];
-
-    foreach ($mergedData as $key => $value) {
-        if (!array_key_exists($key, $data1)) {
-            $ast[$key] = [
-                'status' => 'added',
-                'value' => $value
-            ];
-        } elseif (!array_key_exists($key, $data2)) {
-            $ast[$key] = [
-                'status' => 'deleted',
-                'value' => $value
-            ];
-        } elseif ($value === $data1[$key]) {
-            $ast[$key] = [
-                'status' => 'unchanged',
-                'value' => $value
-            ];
-        } elseif ($value !== $data1[$key]) {
-            $ast[$key] = [
-                'status' => 'modified',
-                'valueBefore' => $data1[$key],
-                'valueAfter' => $value,
-            ];
-        }
-    }
-
-    ksort($ast);
-
-    return $ast;
-}
-*/
-
-/*
-format
-[
-    'key' => [
-        'status' => 'added|deleted|modified|unchanged',
-        'valueBefore' => "scalar|array|object",
-        'valueAfter' => "scalar|array|object"
-    ]
-]
-// property_exists()
-*/
-
 /**
  * Создание промежуточного представления для сравнения
  *
--- tests/BuildAstTest.php
+++ tests/BuildAstTest.php
@@ -1,16 +1,41 @@
 <?php
 
+/**
+ * Тесты buildAst
+ * php version 7.4
+ *
+ * @category Library
+ * @package  Differ\Differ
+ * @author   Valeriy Turbanov <tur.4000@gmail.com>
+ * @license  https://github.com/Tur-4000/hex-lvl2-php/LICENSE MIT
+ * @link     https://github.com/Tur-4000/hex-lvl2-php
+ */
+
 namespace Differ\Differ\Tests;
 
 use PHPUnit\Framework\TestCase;
 
 use function Differ\Differ\Parsers\buildAst;
 
+/**
+ * Класс тестирования buildAst
+ *
+ * @category Library
+ * @package  Differ\Differ
+ * @author   Valeriy Turbanov <tur.4000@gmail.com>
+ * @license  https://github.com/Tur-4000/hex-lvl2-php/LICENSE MIT
+ * @link     https://github.com/Tur-4000/hex-lvl2-php
+ */
 class BuildAstTest extends TestCase
 {
+    /**
+     * Тестирование buildAst
+     *
+     * @return void
+     */
     public function testBuildAst()
     {
-        $expected = [
+        $expected1 = [
             'follow' => [
                 'status' => 'deleted',
                 'value' => false,
@@ -44,6 +69,17 @@ class BuildAstTest extends TestCase
         $data2->verbose = true;
         $data2->host = 'hexlet.io';
 
-        $this->assertEquals($expected, buildAst($data1, $data2));
+        $this->assertEquals($expected1, buildAst($data1, $data2));
+
+        $expected2 = new \stdClass();
+        $expected2->follow = ['state' => 'deleted', 'value' => false,];
+        $expected2->host = ['state' => 'unchanged', 'value' => 'hexlet.io',];
+        $expected2->proxy = ['state' => 'deleted', 'value' => '123.234.53.22',];
+        $expected2->timeout = [
+            'state' => 'modified',
+            'valueBefore' => 50,
+            'valueAfter' => 20
+        ];
+        $expected2->verbose = ['status' => 'added', 'value' => true,];
     }
 }
